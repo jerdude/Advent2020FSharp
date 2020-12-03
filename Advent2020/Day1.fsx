443bdc7e5a29c790884dbe38a5011544ebb8c53b
jerdude <jer157@gmail.com>
2020-12-02 19:13:04 -0500

    Day 1 Part 1 Completed

---
+++ Advent2020/Day1.fsx
@@ -0,0 +1,13 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.IO
+
+let inputs = File.ReadAllLines("Day1Input.txt")
+let inputList = Seq.toList inputs |> Seq.map Int32.Parse
+
+for x in inputList do
+    for y in inputList do
+        if x + y = 2020 then 
+            let answer = x * y |> string
+            printfn "%A" answer
