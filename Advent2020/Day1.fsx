fc6fa10d4e511822e07ce698000c2efccd2c019e
jerdude <jer157@gmail.com>
2020-12-02 19:18:17 -0500

    Piped my inputs

--- Advent2020/Day1.fsx
+++ Advent2020/Day1.fsx
@@ -3,11 +3,12 @@
 open System
 open System.IO
 
-let inputs = File.ReadAllLines("Day1Input.txt")
-let inputList = Seq.toList inputs |> Seq.map Int32.Parse
+let inputs = File.ReadAllLines("Day1Input.txt") 
+            |> Seq.toList 
+            |> Seq.map Int32.Parse
 
-for x in inputList do
-    for y in inputList do
+for x in inputs do
+    for y in inputs do
         if x + y = 2020 then 
             let answer = x * y |> string
             printfn "%A" answer
